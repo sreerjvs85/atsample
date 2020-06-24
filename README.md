1f04b4bde3734df29628e3ceaa7eab2be898c33c
Sree Ravindran <sreerjvs@gmail.com>
2020-06-24 20:30:51 +1200

    read me updated

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # atsample pom framework design
-1. Added maven to mac after resolving dependancy issues.
+1. Added maven to mac after resolving dependency issues.
 2. Created a new maven project and cleaned up.
-3. Added required selenium dependancies in pom.xml and able to build without errors.
+3. Added required selenium dependencies in pom.xml and able to build without errors.
 4. Created new folder structure for Page Object Model. This will have packages for different pages, Browser functions, Common funtions and tests.
--- sample/src/test/java/at/pages/LoginPageObjects.java
+++ sample/src/test/java/at/pages/LoginPageObjects.java
@@ -4,12 +4,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPageObjects {
     WebDriver driver;
 
     public LoginPageObjects(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     @FindBy (how = How.ID, using = "userNameInput")
@@ -20,5 +22,26 @@ public class LoginPageObjects {
 
     @FindBy (how = How.ID, using = "submitButton")
     WebElement btnSubmit;
+
+    @FindBy (how = How.CLASS_NAME, using = "loginErrMsg")
+    WebElement txtErrorMessage;
+
+    public void setTxtUsername (String username) {
+        txtUsername.sendKeys(username);
+    }
+    public void setTxtPassword (String password) {
+        txtPassword.sendKeys(password);
+    }
+    public void clickBtnSubmit() {
+        btnSubmit.click();
+    }
+    public String getTxtErrorMessage() {
+        try {
+            return txtErrorMessage.getText();
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
 
 }
