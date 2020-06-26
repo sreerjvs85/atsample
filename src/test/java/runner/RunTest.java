cb6e32cd51ecb313aab223e2b4ff4bf807d8535d
Sree Ravindran <sreerjvs@gmail.com>
2020-06-26 18:43:26 +1200

    json reports

--- src/test/java/runner/RunTest.java
+++ src/test/java/runner/RunTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"format","json:target/report.json"},
+        plugin = {"json:target/report.json"},
         glue = {"stepDefinitions"},
         features = {"src/test/resources/features"})
 
