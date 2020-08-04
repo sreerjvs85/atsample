108c87aec0700a639d857d616336b29771e048ab
Sree Ravindran <sreerjvs@gmail.com>
2020-08-04 14:58:08 +1200

    updated driver path based on os

--- src/main/java/at/browserLibrary/BrowserFunctions.java
+++ src/main/java/at/browserLibrary/BrowserFunctions.java
@@ -10,8 +10,9 @@ public class BrowserFunctions {
 
     public static WebDriver driver;
     private static String lBrowser;
-    private static String chromePath = "src/test/resources/chromedriver";
-    private static String firefoxPath = "src/test/resources/geckodriver";
+    private static String osName = System.getProperty("os.name");
+    private static String chromePath = "chromedriver";
+    private static String firefoxPath = "geckodriver";
 
     public static WebDriver getDriver(String browser, String URL) {
         lBrowser = browser;
@@ -22,13 +23,24 @@ public class BrowserFunctions {
     }
 
     private static void setlBrowser (String browser) {
-        if (browser.contains("chrome")) {
-            System.setProperty("webdriver.chrome.driver",chromePath);
+        lBrowser = browser;
+        if (lBrowser.contains("chrome")) {
+            System.setProperty("webdriver.chrome.driver", driverPath()+chromePath);
             driver = new ChromeDriver();
-        } else if (browser.contains("firefox")) {
-            System.setProperty("webdriver.gecko.driver",firefoxPath);
+        } else if (lBrowser.contains("firefox")) {
+            System.setProperty("webdriver.gecko.driver", driverPath()+firefoxPath);
             driver = new FirefoxDriver();
         }
+    }
+
+    private static String driverPath() {
+        String driverPath = "src/test/resources/";
+        if (osName.toLowerCase().contains("mac")) {
+            driverPath = driverPath+"mac/";
+        } else {
+            driverPath = driverPath+"windows/";
+        }
+        return driverPath;
     }
 
     public static void quitDriver() throws IOException {
--- src/test/resources/features/loginFeature.feature
+++ src/test/resources/features/loginFeature.feature
@@ -17,8 +17,8 @@ Feature: Login to at and capture error message for failure
     Then I should be able to see my login and check "$28.70" balance
 
   @New
-  Scenario: Login to at in chrome browser with valid login credentials to check the transactions
-    Given I'm on login screen of at using "chrome"
+  Scenario: Login to at in firefox browser with valid login credentials to check the transactions
+    Given I'm on login screen of at using "firefox"
     When I enter username "sreerjvs@gmail.com", password "R@vijaya7" and submit
     Then I click on View Transactions button to see all my previous travels
     And Verify the last transaction's destination as "Tag off : Parnell"
